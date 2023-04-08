4b1a60e1d8eb4eaa489b2f1b2bd55cda7b641d37
Kartal <kartal.cseh@gmail.com>
2023-04-08 19:12:38 +0200

    data visualisation

--- main.py
+++ main.py
@@ -3,8 +3,27 @@ import pandas as pd
 import numpy as np
 import sklearn as skl
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 teams = pd.read_csv('teams.csv')
-print(teams)
-# with open('teams.csv', 'r') as teams:
-#     print(teams)
+# print(teams)
+
+print(teams[['athletes', 'medals']].corr())
+
+sns.lmplot(x='athletes', y='medals', data=teams, fit_reg=True, ci=None)     # Plotting the linear regression between nummbers of athletes and medals
+plt.show()  # this line is needed using pycharm, but not in Jupiter notebook
+# As we can see yes, there is a correlation, so we can use the athletes to predict the number of medals
+
+'''sns.lmplot(x='athletes', y='medals', data=teams, fit_reg=True, ci=None)     # Plotting the linear regression between nummbers of athletes and medals
+plt.show()  # later I want to use this'''
+
+#teams.plot.hist(y='medals')
+#plt.show()
+
+#Remove those data, where are missing values
+
+#teams = [teams.isnull().any(axis=1)]
+#print(teams)
+
+teams = teams.dropna()
+# 2.Building model
